2a795fa2d489ca851c93f7c7c0a05e4c4bd1b2bc
benoit arnoult <contact@benoit-arnoult.xyz>
2020-12-05 15:06:22 +0100

    renamed example file and load config file created

--- util/config.go
+++ util/config.go
@@ -1,6 +1,15 @@
 package util
 
-type Confid struct {
+import "github.com/spf13/viper"
+
+// Config data from ENV file
+type Config struct {
 	DiscordPublicKey string `mapstructure:"DISCORD_PUBLIC_KEY"`
 	DiscordSecretKey string `mapstructure:"DISCORD_SECRET_KEY"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
 }
