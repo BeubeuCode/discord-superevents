59e7bceecefaf861e665816f59db0d176ade48e1
benoit arnoult <contact@benoit-arnoult.xyz>
2020-12-05 15:30:08 +0100

    messageCreateHandler and make bot running

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"discord-superevents/util"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -11,5 +16,33 @@ func main() {
 		fmt.Printf("Error reading config %s", err)
 		return
 	}
+
+	discord, err := discordgo.New("Bot " + config.DiscordSecretKey)
+	if err != nil {
+		err := fmt.Errorf("Error Connecting to API %s", err)
+		fmt.Println(err.Error())
+	}
+
+	discord.AddHandler(messageCreate)
+
 	fmt.Println(config.DiscordPublicKey)
+
+	fmt.Println("Bot is running, press CTRL-C to stop it.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// preventing the bot from replying to itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong !")
+	}
 }
