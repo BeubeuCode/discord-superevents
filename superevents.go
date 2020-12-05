37b017ed782c74359fb27ae8ea9cf69bf4a1d900
benoit arnoult <contact@benoit-arnoult.xyz>
2020-12-05 17:16:05 +0100

    SuperEvent object properties

--- superevents.go
+++ superevents.go
@@ -2,4 +2,9 @@ package main
 
 // SuperEvent struct for holding the superevents data
 type SuperEvent struct {
+	Title       string
+	Quote       string
+	QuoteAuthor string
+	ImageURL    string
+	Subtitle    string
 }
