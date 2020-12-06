5effe1c0c572ab879f3a422721ae749b3641a177
benoit arnoult <contact@benoit-arnoult.xyz>
2020-12-06 11:10:04 +0100

    fixed naming errors

--- commands/createSuperEvents.go
+++ commands/createSuperEvents.go
@@ -1,7 +1,33 @@
 package commands
 
-import "github.com/Lukaesebrot/dgc"
+import (
+	"context"
+	"discord-superevents/util"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"github.com/Lukaesebrot/dgc"
+	"google.golang.org/api/option"
+)
 
 func CreateSuperEvent(ctx *dgc.Ctx) {
-	ctx.RespondText("PlaceHolder")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		fmt.Printf("Error reading config %s", err)
+		return
+	}
+	context := context.Background()
+	sa := option.WithCredentialsFile(config.FirebaseKeyPath)
+	app, err := firebase.NewApp(context, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(context)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+	ctx.RespondText("")
 }
